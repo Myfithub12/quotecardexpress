c81160d5bf146bca0c2770c8ea1fa288c820f5f1
Mark Hollrah <mhollrah0001@gmail.com>
2026-02-28 15:03:15 -0500

    02/28/26

--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,13 @@ async function getRandomImage() {
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
         const response = await fetch(endpoint);
-        const returnedData = await response.json()
-        console.log(returnedData)
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
